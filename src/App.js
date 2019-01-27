37184af9163a788baabddeb0e35d9e57c4c9f3df
yongmankim <heaven0223@naver.com>
2019-01-27 17:37:11 +0900

    complete!!

--- src/App.js
+++ src/App.js
@@ -7,48 +7,42 @@ class App extends Component {
   // component cycle
   //render : componentWillMount()  ->  render()  ->  componentWithMount()
   //update : componentWillRecieveProps()  ->  shouldCompoenetUpdate()  ->  componentWillUpdate()  ->  render()  ->componentDidUpdate
-  state = {
-    
-  }
+  state = {}
   componentDidMount(){ 
-    setTimeout(()=>{
-      this.setState({
-        movies : [
-          {
-            title : "Matrix",
-            poster : "https://upload.wikimedia.org/wikipedia/en/b/bb/Oldboy_2013_film_poster.jpg"
-          },
-          {
-            title : "Full metal",
-            poster : "https://upload.wikimedia.org/wikipedia/en/b/bb/Oldboy_2013_film_poster.jpg"
-          },
-          {
-            title : "Old boy",
-            poster: "https://upload.wikimedia.org/wikipedia/en/b/bb/Oldboy_2013_film_poster.jpg"
-          },
-          {
-            title : "Star wars",
-            poster : "https://upload.wikimedia.org/wikipedia/en/b/bb/Oldboy_2013_film_poster.jpg"
-          },
-          {
-            title:"Transfomer",
-            poster: "https://upload.wikimedia.org/wikipedia/ko/thumb/a/a8/%ED%8A%B8%EB%9E%9C%EC%8A%A4%ED%8F%AC%EB%A8%B8_%EC%B5%9C%ED%9B%84%EC%9D%98_%EA%B8%B0%EC%82%AC.jpg/250px-%ED%8A%B8%EB%9E%9C%EC%8A%A4%ED%8F%AC%EB%A8%B8_%EC%B5%9C%ED%9B%84%EC%9D%98_%EA%B8%B0%EC%82%AC.jpg"
-          }
-        ]
-      })
-    }, 3000)
+    this._getMovies()
   }
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index}/>
+    const movies = this.state.movies.map(movie => {
+      return <Movie 
+        title={movie.title_english} 
+        poster={movie.medium_cover_image} 
+        key={movie.id} 
+        genres={movie.genres}
+        synopsis = {movie.synopsis}
+      />
     })
     return movies
   }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+    
+  }
+  _callApi = () => {
+    return fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
+    .then(response => response.json())
+    .then(json =>  json.data.movies)
+    .catch(err=>console.log(err))
+  }
   render() {
+    const { movies } = this.state;
     return (
-      <div className="App">
-        {this.state.movies ? this._renderMovies() : "loding"}
-      </div>
+      <div className={movies ? "App" : "App--loading"}>
+      {movies ? this._renderMovies() : "Loading"}
+    </div>
     )
   }
 }
